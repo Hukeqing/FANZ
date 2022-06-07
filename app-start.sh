5c4826872764c5559a11932141e9698e6be667e5
shiroha <keqing.hu@icloud.com>
2022-06-07 21:56:21 +0800

    增加了自动更新脚本

--- app-start.sh
+++ app-start.sh
@@ -1,2 +1,4 @@
 cd /root/FUCK-NCO-ZJGSU
+git pull
+chmod +x app-start.sh
 /usr/bin/python3 app.py ticket >> app-out.txt
--- app-yzy.sh
+++ app-yzy.sh
@@ -1,2 +1,4 @@
 cd /root/FUCK-NCO-ZJGSU
+git pull
+chmod +x app-yzy.sh
 /usr/bin/python3 app.py yzy >> app-out.txt
